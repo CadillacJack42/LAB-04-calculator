5b9b93579a6d15735c22b4c479186e3c8d4cad44
James Keicher <jamesdkeicher@gmail.com>
2021-10-07 13:48:01 -0700

    Completed calculator app

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 // import functions
-import { add } from './calculations.js';
-// reference needed DOM elements
+import { add, sub, multiply, divide } from './calculations.js';
+
+// Addition elements
+const addition1 = document.getElementById('addition1');
+const addition2 = document.getElementById('addition2');
+const addButton = document.getElementById('add-button');
+const addResult = document.getElementById('add-result');
+
+addButton.addEventListener('click', () => {
+    let result = add(+addition1.value, +addition2.value);
+    addResult.textContent = result;
+});
+
+// Subtraction elements
+const subtraction1 = document.getElementById('subtraction1');
+const subtraction2 = document.getElementById('subtraction2');
+const subButton = document.getElementById('sub-button');
+const subResult = document.getElementById('sub-result');
+
+subButton.addEventListener('click', () => {
+    let result = sub(+subtraction1.value, +subtraction2.value);
+    subResult.textContent = result;
+});
 
 
-// set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+// Multiplication elements
+const multiplication1 = document.getElementById('multiplication1');
+const multiplication2 = document.getElementById('multiplication2');
+const multButton = document.getElementById('mult-button');
+const multResult = document.getElementById('mult-result');
+
+multButton.addEventListener('click', () => {
+    let result = multiply(+multiplication1.value, +multiplication2.value);
+    multResult.textContent = result;
+});
+
+
+// Division Elements
+const division1 = document.getElementById('division1');
+const division2 = document.getElementById('division2');
+const divButton = document.getElementById('div-button');
+const divResult = document.getElementById('div-result');
+
+divButton.addEventListener('click', () => {
+    let result = divide(+division1.value, +division2.value);
+    divResult.textContent = result;
+});
+
+
+
--- test/tests.js
+++ test/tests.js
@@ -3,71 +3,45 @@ import { add, sub, multiply, divide } from '../calculations.js';
 
 const test = QUnit.test;
 
-// name your test by what it is testing
+
 test('add two numbers 5 and 3', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
     
     const x = 5;
     const y = 3;
     const expected = 8;
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = add(x, y); // use your function here
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    const actual = add(x, y);
     expect.equal(actual, expected);
 });
 
 test('subtract two numbers 10 and 5', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    
     const x = 10;
     const y = 5;
     const expected = 5;
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = sub(x, y); // use your function here
+    const actual = sub(x, y);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
 
 test('multiply two numbers 10 and 42', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
     
     const x = 10;
     const y = 42;
     const expected = 420;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = multiply(x, y); // use your function here
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    const actual = multiply(x, y);
+
     expect.equal(actual, expected);
 });
 
 test('divide two numbers 100 and 10', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
     
     const x = 100;
     const y = 10;
     const expected = 10;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = divide(x, y); // use your function here
+   
+    const actual = divide(x, y);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
